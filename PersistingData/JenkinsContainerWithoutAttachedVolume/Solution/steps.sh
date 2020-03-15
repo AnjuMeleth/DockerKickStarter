48dff4bcdb2c5be92ec9c4ffcebd008ee3140f85
AnjuMeleth <anjurose.d@gmail.com>
2020-03-15 11:59:45 +0530

    Modified without volume

--- PersistingData/JenkinsContainerWithoutAttachedVolume/Solution/steps.sh
+++ PersistingData/JenkinsContainerWithoutAttachedVolume/Solution/steps.sh
@@ -6,8 +6,12 @@ docker run --name MyJenkins -p 5000:5000 -p 8080:8080 jenkins
 docker ps
 docker exec -it MyJenkins /bin/bash
 docker ps
-docker cp MyJenkins:/var/jenkins_home /var/lib/docker/volumes/MyVol/_data
+docker cp MyJenkins:/var/jenkins_home ./data
 docker stop MyJenkins
 docker rm MyJenkins
 
-# create a new containedocker run --name MyJenkins -p 5000:5000 -p 8080:8080 jenkinsr
+# create a new containedocker run --name MyJenkins -p 5000:5000 -p 8080:8080 jenkins
+# change directory permission
+chown -R 1000 data
+docker run -p 50000:50000 -p 8080:8080 -v ~/data/jenkins_home:/var/jenkins_home --name DataJenkins jenkins/jenkins
+# / or ~/ bind mounts
